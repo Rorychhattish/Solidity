b52477fd724008224c83a051d82467c2c9e16ab3
Chhatish kumar Sah <rorychhattish@gmail.com>
2026-05-25 22:45:59 +0545

    feat: add public address variables

---
+++ Address Interactions/ESCROW/1_Setup.sol
@@ -0,0 +1,13 @@
+// Goal: Addresses
+// Create three public storage variables for the addresses of the depositor, beneficiary and arbiter.
+
+
+
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+contract Escrow {
+    address public depositor;
+    address public beneficiary;
+    address public arbiter;
+}
