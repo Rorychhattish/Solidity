78d3f04ed482582c15fbe85a426db99b8afe1d5e
Chhatish kumar Sah <rorychhattish@gmail.com>
2026-07-03 14:38:05 +0545

    feat: implement connection mapping between addresses

---
+++ Types/MAPPINGS/6_Nested_Maps.sol
@@ -0,0 +1,23 @@
+// Goal: Make Connections
+// Create a public mapping called connections which will map an address to a mapping of an address to a ConnectionTypes enum value.
+// In theconnectWith function, create a connection from the msg.sender to the other address.
+
+
+
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract Contract {
+	enum ConnectionTypes { 
+		Unacquainted,
+		Friend,
+		Family
+	}
+	
+	mapping(address=>mapping(address=>ConnectionTypes)) public connections;
+
+	function connectWith(address other, ConnectionTypes connectionType) external {
+		connections[msg.sender][other] = connectionType;
+	}
+}
